cb677b049749df6aad52de8230089d183a704a3f
mandrocker <mandrocker@gmail.com>
2015-05-23 18:50:14 +0100

    added code implementation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## the make cache matrix creates a wrap object around the input matrxi
+## get and set are storing the input matrix while getInverted 
+##    and setInverted are storing the calculated inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ix <- NULL
+  get <- function() x
+  set <- function(y){
+    x <<- y
+    ix <- NULL
+  }
+  getInverted <- function() ix
+  
+  setInverted <- function(inverted){
+    ix <<- inverted
+  }
+  
+  list(set = set, get = get, setInverted = setInverted, 
+       getInverted = getInverted)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve returns the inverted matrix if already cached in the wrapped matrix.
+##  otherwise calculates the inverted matrix and stores it in the wrapper for future use
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    ix <- x$getInverted()
+    if(!is.null(ix)){
+      message("retrieving from the cache")
+      return(ix)
+    }
+    data <- x$get()
+    ix <- solve(data, ...)
+    x$setInverted(ix)
+    ix
 }
